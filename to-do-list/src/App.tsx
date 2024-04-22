af4cbb0a843ef57b128ae80e781ec697617ead99
Mi Yeon Lee <immioriii@gmail.com>
2024-04-22 23:05:17 +0900

    Squashed commit of the following:
    commit 4a8c6da9bcfb4688b6b809ad6bae8cec8abfe9c4
    Author: Mi Yeon Lee <immioriii@gmail.com>
    Date:   Mon Apr 22 23:03:20 2024 +0900
    
        feat: remove unused file
    
    commit c072730ef0db41e500dc29805b401de43c58fb8c
    Author: Mi Yeon Lee <immioriii@gmail.com>
    Date:   Mon Apr 22 23:01:29 2024 +0900
    
        feat: add feature to create

--- to-do-list/src/App.tsx
+++ to-do-list/src/App.tsx
@@ -1,13 +1,56 @@
+import { useReducer, useRef, useState } from 'react';
 import './App.css';
 import Editor from './components/editor';
 import Header from './components/header';
 import List from './components/list';
 
+interface mockModel {
+  id: number;
+  isDone: boolean;
+  content: string;
+  date: Date;
+}
+
+const mockData: mockModel[] = [
+  {
+    id: 0,
+    isDone: false,
+    content: 'react 공부하기',
+    date: new Date(),
+  },
+  {
+    id: 1,
+    isDone: false,
+    content: '수영 가기',
+    date: new Date(),
+  },
+  {
+    id: 2,
+    isDone: false,
+    content: '러닝',
+    date: new Date(),
+  },
+];
+
 function App() {
+  const [todos, setTodos] = useState<mockModel[]>(mockData);
+  const idRef = useRef<number>(3);
+
+  const onCreate = (content: string) => {
+    const newTodo: mockModel = {
+      id: idRef.current++,
+      isDone: false,
+      content,
+      date: new Date(),
+    };
+
+    setTodos([newTodo, ...todos]);
+  };
+
   return (
     <div className="App">
       <Header />
-      <Editor />
+      <Editor onCreate={onCreate} />
       <List />
     </div>
   );
--- to-do-list/src/components/editor.tsx
+++ to-do-list/src/components/editor.tsx
@@ -1,10 +1,43 @@
+import { ChangeEvent, KeyboardEvent, useRef, useState } from 'react';
 import './editor.css';
 
-export default function Editor() {
+interface Props {
+  onCreate: (content: string) => void;
+}
+export default function Editor({ onCreate }: Props) {
+  const [content, setContent] = useState<string>('');
+  const contentRef = useRef<HTMLInputElement>(null);
+
+  const onChangeContent = (e: ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target;
+    setContent(value);
+  };
+
+  const onKeyDown = (e: KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      onSubmit();
+    }
+  };
+
+  const onSubmit = () => {
+    if (!content) {
+      contentRef.current?.focus();
+      return;
+    }
+    onCreate(content);
+    setContent('');
+  };
+
   return (
     <div className="Editor">
-      <input placeholder="새로운 ToDo..." />
-      <button>추가</button>
+      <input
+        ref={contentRef}
+        value={content}
+        onKeyDown={onKeyDown}
+        onChange={onChangeContent}
+        placeholder="새로운 Todo..."
+      />
+      <button onClick={onSubmit}>추가</button>
     </div>
   );
 }
